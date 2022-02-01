8f3e42d2fdf34d95999712ce3c91f489c58c5453
= Jonathan Hansel Awo <= jonhan17@gmail.com>
2022-02-01 09:20:03 +0000

    Working on signin

--- pages/home.js
+++ pages/home.js
@@ -6,29 +6,36 @@ import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import Head from "next/head";
+import { getProviders, signIn } from "next-auth/react";
 
-const Home = () => {
+const Home = ({providers}) => {
+
     return (
         <div className="">
             <header className="flex justify-around items-center py-4">
                 <div className="relative w-36 h-10">
-                    <Image src="https://rb.gy/dcmipu" layout="fill" objectFit="contain" />
+                    <Image src="https://rb.gy/mju7xu" layout="fill" objectFit="contain" />
                 </div>
-                <div classname="flex items-center sm:divide-x divide-gray-300">
+                <div classname="flex flex-row items-center sm:divide-x divide-gray-300">
                     <div className="hidden sm:flex space-x-8 pr-4">
                         <HeaderLink Icon={ExploreIcon} text="Discover" />
                         <HeaderLink Icon={GroupIcon} text="Connections" />
                         <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
                         <HeaderLink Icon={BusinessCenterIcon} text="HaLiBee" />
                     </div>
-                    <div classname="pl-4">
-                        <button
-                            className="text-blue-700 font-semibold rounded-full border border-blue-700 px-3 py-1.5 transition-all hover:border-2"
-                        // onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-                        >
-                            Sign in
-                        </button>
-                    </div>
+                    
+                    {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <div className="pl-4">
+                <button
+                  className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
+                  onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                >
+                  Sign in
+                </button>
+              </div>
+            </div>
+          ))}
                 </div>
             </header>
 
@@ -36,7 +43,7 @@ const Home = () => {
                 className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto"
             >
                 <div className="space-y-6 xl:space-y-10">
-                    <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
+                    <h1 className="text-3xl md:text-5xl text-amber-700 max-w-xl !leading-snug pl-4 xl:pl-0">
                         Welcome to your HaLi community
                     </h1>
                     <div className="space-y-4">
@@ -64,3 +71,13 @@ const Home = () => {
 };
 
 export default Home;
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders();
+  
+    return {
+      props: {
+        providers,
+      },
+    };
+  }
